9bc6b11007b944dcf6ba3d3a5b93a74c7dc98f97
wjrobinson <wjrobinson1992@gmail.com>
2023-02-13 20:23:26 -0700

    Fixed formatting of dollar amount

--- 2 - Tip Calculator/tip-calculator.py
+++ 2 - Tip Calculator/tip-calculator.py
@@ -9,5 +9,6 @@ total_tip_amount = bill * tip_percentage
 total_bill_amount = bill + total_tip_amount
 bill_per_person = total_bill_amount / people
 final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 
 print(f"Each person should pay ${final_amount}")
